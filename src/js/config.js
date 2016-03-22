8916724b9c3471155711ea116ad8cf7ee8e12b63
adriennn <adrien.vetterli@gmail.com>
2016-03-22 18:33:38 +0200

    update port number in config.js

--- src/js/config.js
+++ src/js/config.js
@@ -6,7 +6,7 @@ var api = {
   // Backend
   // TODO the server urls should sit in .env
   // server: 'https://dev.garbagepla.net:8081/api', // the unstable api
-  server: 'https://dev.garbagepla.net:8080/api',
+  server: 'https://dev.garbagepla.net:8443/api',
 
   // API entry points
   /* User */
